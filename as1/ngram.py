a3e0ef4461e3e2cda6056f64bcc34ed2c58b7096
Optictrollz <suhasm@me.com>
2020-01-22 15:29:41 -0800

    comments

--- as1/ngram.py
+++ as1/ngram.py
@@ -3,26 +3,29 @@ from models import unigram
 from models import bigram
 
 UNK = "UNK"
-
+#open file and return array of content
 def openF(f):
     with open(f, "r", encoding="utf8") as myfile:
         data=myfile.read().replace("\n", " STOP ")
     arr = data.split()
     return arr
-
+#data sets used for processing
 train = openF("1b_benchmark.train.tokens")
 val = openF("1b_benchmark.dev.tokens")
 test = openF("1b_benchmark.test.tokens")
 
+#replace tokens that happened less that three times with "UNKS"
 trainC = Counter(train)
 train = [n if trainC[n] >= 3 else "UNK" for n in train]
 train.insert(0, "START")
 
+
+#unigram
 modelU = unigram(train)
 print(modelU.perp(train))
 print(modelU.perp(val))
 print(modelU.perp(test))
-
+#bigram
 modelB = bigram(train)
 print(modelB.perp(train))
 print(modelB.perp(val))
