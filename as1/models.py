184d5ca0eb7f9d2911c399fe46abeab1cd35bb28
poilpy <colinmurphy97@gmail.com>
2020-01-24 12:56:36 -0800

    smoothy

--- as1/models.py
+++ as1/models.py
@@ -8,13 +8,16 @@ class unigram:
     def __init__(self, text):
         self.freq = dict()
         self.text = text
-        self.total = len(text) - 2
+        self.total = 0
         for word in self.text:
             self.freq[word] = self.freq.get(word, 0) + 1
+            if word != stop or word != start: 
+                self.total += 1
         self.numUnique = len(self.freq) - 2
+
 #calculate word probability from frequency
     def wordProb(self, word):
-        num = self.freq[word]
+        num = self.freq.get(word, 1)
         den = self.total
         return num/den
 
@@ -22,10 +25,9 @@ class unigram:
     def perp(self, test):
         logProb = 0
         for word in test:
-            if word in self.freq:
-                logProb -= np.log2(self.wordProb(word))
-            else:
-                logProb -= np.log2(self.wordProb("UNK"))
+            if word not in self.freq:
+                word = "UNK"
+            logProb -= np.log2(self.wordProb(word))
         logProb = logProb/len(test)
         return np.power(2, logProb)
         
@@ -34,7 +36,6 @@ class bigram:
     def __init__(self, text):
         self.freq = dict()
         self.text = text
-        self.total = len(text) - 2
         self.prevWord = None
         self.uni = unigram(text)
         for word in self.text:
@@ -42,11 +43,13 @@ class bigram:
                 self.freq[(self.prevWord, word)] = self.freq.get((self.prevWord, word), 0) + 1
             self.prevWord = word
         self.numUnique = len(self.freq) - 2
+
 #calculate word probability from frequency
     def wordProb(self, prevWord, word):
-        num = self.freq.get((prevWord, word), 0)
-        den = self.uni.freq[prevWord]
+        num = self.freq.get((prevWord, word), 1)
+        den = self.uni.freq.get(prevWord, 1)
         return num/den
+
   #calculate perplexity using formula 
     def perp(self, test):
         logProb = 0
@@ -55,19 +58,21 @@ class bigram:
             if word not in self.uni.freq:
                 word = "UNK"
             if self.prevWord != None:
-                logProb -= np.log2(self.wordProb(self.prevWord, word) if self.wordProb(self.prevWord, word) != 0 else 1)
+                logProb -= np.log2(self.wordProb(self.prevWord, word))
             self.prevWord = word
         logProb = logProb/len(test)
         return np.power(2, logProb)
 
 class trigram:
-    def __init__(self, text):
+    def __init__(self, text, l1=1, l2=2, l3=1):
         self.freq = dict()
         self.text = text
-        self.total = len(text) - 2
         self.prevWord = None
         self.prevPrevWord = None
         self.bi = bigram(text)
+        self.l1 = l1
+        self.l2 = l2
+        self.l3 = l3
         for word in self.text:
             if self.prevWord != None:
                 if self.prevPrevWord != None:
@@ -77,14 +82,8 @@ class trigram:
         self.numUnique = len(self.freq) - 2
 
     def wordProb(self, prevPrevWord, prevWord, word):
-        num = self.freq.get((prevPrevWord, prevWord, word), 0)
-        den = self.bi.freq.get((prevPrevWord, prevWord), 0)
-        if den == 0 or num == 0:
-            # print(word)
-            # print(num)
-            # print("/")
-            # print(den)
-            return 1
+        num = self.freq.get((prevPrevWord, prevWord, word), 1)
+        den = self.bi.freq.get((prevPrevWord, prevWord), 1)
         return np.float(num/den)
     
     def perp(self, test):
@@ -96,7 +95,13 @@ class trigram:
                 word = "UNK"
             if self.prevWord != None:
                 if self.prevPrevWord != None:
-                    logProb -= np.log2(self.wordProb(self.prevPrevWord, self.prevWord, word))
+                    if self.l3 != 1:
+                        logProb -= np.log2(self.l3 * self.wordProb(self.prevPrevWord, self.prevWord, word)
+                            + self.l2 * self.bi.wordProb(self.prevWord, word)
+                            + self.l1 * self.bi.uni.wordProb(word)
+                            )
+                    else:
+                        logProb -= np.log2(self.wordProb(self.prevPrevWord, self.prevWord, word))
                 self.prevPrevWord = self.prevWord
             self.prevWord = word
         logProb = logProb/len(test)
--- as1/ngram.py
+++ as1/ngram.py
@@ -7,7 +7,7 @@ UNK = "UNK"
 #open file and return array of content
 def openF(f):
     with open(f, "r", encoding="utf8") as myfile:
-        data=myfile.read().replace("\n", " STOP Start")
+        data=myfile.read().replace("\n", " STOP Start ")
     arr = data.split()
     arr.insert(0, "START")
     return arr[:-1]
@@ -19,24 +19,58 @@ test = openF("1b_benchmark.test.tokens")
 
 #replace tokens that happened less that three times with "UNKS"
 trainC = Counter(train)
-train = [n if trainC[n] >= 3 else "UNK" for n in train]
+train = [n if trainC[n] >= 3 else UNK for n in train]
 
 #unigram
-print("Unigram model preplexity: Train/Val/Test")
+print("Unigram model preplexity:")
 modelU = unigram(train)
-print(modelU.perp(train))
-print(modelU.perp(val))
-print(modelU.perp(test))
+print("Train = ", modelU.perp(train))
+print("Val = ", modelU.perp(val))
+print("Test = ", modelU.perp(test), "\n")
 
 #bigram
-print("Bigram model preplexity: Train/Val/Test")
+print("Bigram model preplexity:")
 modelB = bigram(train)
-print(modelB.perp(train))
-print(modelB.perp(val))
-print(modelB.perp(test))
+print("Train = ", modelB.perp(train))
+print("Val = ", modelB.perp(val))
+print("Test = ", modelB.perp(test), "\n")
 
-print("Trigram model preplexity: Train/Val/Test")
+#trigram
+print("Trigram model preplexity:")
 modelT = trigram(train)
-print(modelT.perp(train))
-print(modelT.perp(val))
-print(modelT.perp(test))
+print("Train = ", modelT.perp(train))
+print("val = ", modelT.perp(val))
+print("Test = ", modelT.perp(test), "\n")
+
+smoo = trigram(train, .1, .3, .6)
+print("Perplexity for l1=.1, l2=.3, l3=.6")
+print("Train = ", smoo.perp(train))
+print("Val = ", smoo.perp(val))
+print("Test = ", smoo.perp(test))
+smoo = trigram(train, .2, .2, .6)
+print("Perplexity for l1=.2, l2=.2 l3=.6")
+print("Train = ", smoo.perp(train))
+print("Val = ", smoo.perp(val))
+print("Test = ", smoo.perp(test))
+smoo = trigram(train, .1, .1, .8)
+print("Perplexity for l1=.1, l2=.1, l3=.8")
+print("Train = ", smoo.perp(train))
+print("Val = ", smoo.perp(val))
+print("Test = ", smoo.perp(test))
+smoo = trigram(train, .8, .1, .1)
+print("Perplexity for l1=.8, l2=.1, l3=.1")
+print("Train = ", smoo.perp(train))
+print("Val = ", smoo.perp(val))
+print("Test = ", smoo.perp(test))
+smoo = trigram(train, .1, .8, .1)
+print("Perplexity for l1=.1, l2=.8, l3=.1")
+print("Train = ", smoo.perp(train))
+print("Val = ", smoo.perp(val))
+print("Test = ", smoo.perp(test))
+
+
+print("Trigram model preplexity (Half):")
+modelT = trigram(train[:(len(train)/2 +1)])
+print("Train = ", modelT.perp(train))
+print("val = ", modelT.perp(val))
+print("Test = ", modelT.perp(test), "\n")
